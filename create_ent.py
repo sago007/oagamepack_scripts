9c5783660752c8670b4d4c0d4f15506a8c6e2fc5
Poul Sander <poul@unity3d.com>
2017-11-22 19:09:52 +0100

    Somewhat "default" support. Integers are printed as reals

--- create_ent.py
+++ create_ent.py
@@ -66,6 +66,7 @@ def printKeys(item_name):
                 hasKey = keyLine[item]
                 #The hasKey == hasKey us used to check for NaN. Blank fields are NaNs in Pandas
                 if (hasKey and hasKey == hasKey):
+                    defaultText = ""
                     basename = item
                     basekey = key_texts[item]["basekey"]
                     if (isinstance(basekey, str) and len(basekey)>0):
@@ -74,7 +75,12 @@ def printKeys(item_name):
                     fullname = key_texts[item]["fullname"]
                     if (isinstance(fullname, str) and len(fullname)>0):
                         name = fullname
-                    print("<"+key_texts[item]["type"]+" key=\""+basename+"\" name=\""+name+"\">"+key_texts[item]["text"]+"</"+key_texts[item]["type"]+">")
+                    try:
+                        defaultTextActual = keyLine[item+"_default"]
+                        defaultText = " Default: "+str(defaultTextActual)
+                    except:
+                        defaultText = ""
+                    print("<"+key_texts[item]["type"]+" key=\""+basename+"\" name=\""+name+"\">"+key_texts[item]["text"]+defaultText+"</"+key_texts[item]["type"]+">")
                     
 def printNotes(item_name):
     for item in note_texts:
